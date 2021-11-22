9ebcfe62fbb23ab6d23a6aa29479ae3d18aaaa6b
kobayashiryou <chiri.parihead@gmail.com>
2021-11-22 09:36:47 +0900

    create:  header

---
+++ frontend/src/components/atoms/ScrollTop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span @click="onClick">戻る<span>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const onClick = () => {
+      return window.scroll(0,0);
+    }
+    return onClick
+  },
+})
+</script>
+
+<style lang="sass" scoped>
+
+</style>
